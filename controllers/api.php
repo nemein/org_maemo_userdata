7113d4b54550aa99113c87ee7bc37445521d4467
Alexey Zakhlestin <indeyets@gmail.com>
2009-12-08 20:25:34 +0000

    controller-code

--- controllers/api.php
+++ controllers/api.php
@@ -20,29 +20,144 @@ class org_maemo_userdata_controllers_api
 
     public function get_index()
     {
+        // nothing to do here
     }
 
     public function get_transactions(array $args)
     {
+        $qb = new midgard_query_builder('org_maemo_userdata_transaction');
+        $qb->add_constraint('apiuuid', '=', $args['uuid']);
+
+        if ($qb->count() != 1)
+        {
+            $this->data = array(false);
+        }
+        else
+        {
+            $result = $qb->execute();
+            $since_trx = $result[0];
+
+            $qb = new midgard_query_builder('org_maemo_userdata_transaction');
+            $qb->add_constraint('metadata.created', '>', $since_trx->metadata_created);
+            // FIXME: add order
+            $trxs = $qb->execute();
+
+            $this->data = array();
+
+            foreach ($trxs as $trx)
+            {
+                $user = org_maemo_userdata::userByUuid($trx->useruuid);
+
+                $this->data[] = array(
+                    'uuid'      => $trx->apiuuid,
+                    'action'    => $trx->action,
+                    'timestamp' => $trx->created->format(DATE_W3C),
+                    'data'      => org_maemo_userdata::personToArray($user),
+                );
+            }
+        }
     }
 
     public function get_userByLogin(array $args)
     {
+        $qb = new midgard_query_builder('org_maemo_userdata_person');
+        $qb->add_constraint('username', '=', $args['login']);
+
+        if ($qb->count() != 1)
+        {
+            $this->data = array(false);
+        }
+        else
+        {
+            $result = $qb->execute();
+            $this->data = json_encode(org_maemo_userdata::personToArray($result[0]));
+        }
     }
 
     public function get_userByEmail(array $args)
     {
+        $qb = new midgard_query_builder('org_maemo_userdata_person');
+        $qb->add_constraint('email', '=', $args['email']);
+
+        if ($qb->count() != 1)
+        {
+            $this->data = array(false);
+        }
+        else
+        {
+            $result = $qb->execute();
+            $this->data = json_encode(org_maemo_userdata::personToArray($result[0]));
+        }
     }
 
     public function get_userByUuid(array $args)
     {
+        $user = org_maemo_userdata::userByUuid($args['uuid']);
+
+        if (null === $user)
+        {
+            $this->data = array(false);
+        }
+        else
+        {
+            $this->data = json_encode(org_maemo_userdata::personToArray($result[0]));
+        }
     }
 
     public function post_userByUuid(array $args)
     {
+        $raw = file_get_contents('php://input');
+        $user_data = json_decode($raw, true);
+
+        $user = org_maemo_userdata::userByUuid($args['uuid']);
+
+        if (null === $user)
+        {
+            $this->data = array(false);
+        }
+        else
+        {
+            $_allowed = org_maemo_userdata::getListOfUserFields();
+            foreach ($user_data as $k => $v)
+            {
+                if (in_array($k, $_allowed))
+                {
+                    $obj->$k = $v;
+                }
+            }
+            $user->update();
+
+            org_maemo_userdata::registerTransaction($obj, 'update');
+
+            $this->data = array(true);
+        }
     }
 
     public function post_createUser(array $args)
     {
+        $raw = file_get_contents('php://input');
+        $user_data = json_decode($raw, true);
+
+        if (!isset($user_data['username']) or !isset($user_data['password']))
+        {
+            $this->data = array(false);
+            return;
+        }
+
+        $obj = new org_maemo_userdata_person();
+        $obj->apiuuid = org_maemo_userdata::generate_UUID();
+        $_allowed = org_maemo_userdata::getListOfUserFields();
+        foreach ($user_data as $k => $v)
+        {
+            if (in_array($k, $_allowed))
+            {
+                $obj->$k = $v;
+            }
+        }
+        $obj->create();
+
+        org_maemo_userdata::registerTransaction($obj, 'create');
+
+        $this->data = array('uuid' => $obj->apiuuid);
     }
 }
--- interface.php
+++ interface.php
@@ -13,4 +13,147 @@
  */
 class org_maemo_userdata extends midgardmvc_core_component_baseclass
 {
+    /**
+     * @brief Generates a Universally Unique IDentifier, version 4.
+     *
+     * This function generates a truly random UUID. The built in CakePHP String::uuid() function
+     * is not cryptographically secure. You should uses this function instead.
+     *
+     * @see http://tools.ietf.org/html/rfc4122#section-4.4
+     * @see http://en.wikipedia.org/wiki/UUID
+     * @return string A UUID, made up of 32 hex digits and 4 hyphens.
+     * @author sean at seancolombo dot com (via http://docs.php.net/uniqid#88023)
+     */
+    public static function generate_UUID()
+    {
+        $pr_bits = null;
+        $fp = @fopen('/dev/urandom','rb');
+
+        if ($fp !== false)
+        {
+            $pr_bits .= @fread($fp, 16);
+            @fclose($fp);
+        }
+        else
+        {
+            // If /dev/urandom isn't available (eg: in non-unix systems), use mt_rand().
+            $pr_bits = "";
+            for ($cnt=0; $cnt < 16; $cnt++)
+            {
+                $pr_bits .= chr(mt_rand(0, 255));
+            }
+        }
+
+        $time_low = bin2hex(substr($pr_bits,0, 4));
+        $time_mid = bin2hex(substr($pr_bits,4, 2));
+        $time_hi_and_version = bin2hex(substr($pr_bits,6, 2));
+        $clock_seq_hi_and_reserved = bin2hex(substr($pr_bits,8, 2));
+        $node = bin2hex(substr($pr_bits,10, 6));
+
+        /**
+         * Set the four most significant bits (bits 12 through 15) of the
+         * time_hi_and_version field to the 4-bit version number from
+         * Section 4.1.3.
+         * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
+         */
+        $time_hi_and_version = hexdec($time_hi_and_version);
+        $time_hi_and_version = $time_hi_and_version >> 4;
+        $time_hi_and_version = $time_hi_and_version | 0x4000;
+
+        /**
+         * Set the two most significant bits (bits 6 and 7) of the
+         * clock_seq_hi_and_reserved to zero and one, respectively.
+         */
+        $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
+        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
+        $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
+
+        return sprintf(
+            '%08s-%04s-%04x-%04x-%012s',
+            $time_low, $time_mid, $time_hi_and_version,
+            $clock_seq_hi_and_reserved,
+            $node
+        );
+    }
+
+    public static function userByUuid($uuid)
+    {
+        $qb = new midgard_query_builder('org_maemo_userdata_person');
+        $qb->add_constraint('apiuuid', '=', $uuid);
+
+        if ($qb->count() != 1)
+        {
+            return null;
+        }
+        else
+        {
+            $result = $qb->execute();
+            return $result[0];
+        }
+    }
+
+    public static function getListOfUserFields()
+    {
+        // seems like midgard doesn't export these dynamically.
+        // so, list needs to be manually updated
+        return array(
+            "morgid",
+            "garageid",
+            "talkid",
+            "username",
+            "password",
+            "joindate",
+            "title",
+            "firstname",
+            "lastname",
+            "birthdate",
+            "street",
+            "postcode",
+            "city",
+            "country",
+            "ccode",
+            "email",
+            "phone",
+            "fax",
+            "homepage",
+            "jabber",
+            "icq",
+            "aim",
+            "yahoo",
+            "msn",
+            "skype",
+        );
+    }
+
+    public static function personToArray(org_maemo_userdata_person $person)
+    {
+        $fields = org_maemo_userdata::getListOfUserFields();
+
+        $result = array('uuid' => $person->apiuuid);
+
+        foreach ($fields as $field)
+        {
+            $value = $person->$field;
+
+            if ($value instanceof midgard_datetime)
+            {
+                $result[$field] = $value->format(DATE_W3C);
+            }
+            else
+            {
+                $result[$field] = $value;
+            }
+        }
+
+        return $result;
+    }
+
+    public static function registerTransaction(org_maemo_userdata_person $person, $action)
+    {
+        $trx = new org_maemo_userdata_transaction();
+        $trx->apiuuid = org_maemo_userdata::generate_UUID();
+        $trx->useruuid = $person->apiuuid;
+        $trx->action = $action;
+        $trx->create();
+    }
 }
